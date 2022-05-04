36324ce90c37a92e7dafb331c6c082ddc654ef50
marisobreiro <marianasobreiro93@gmail.com>
2022-05-04 13:43:48 -0300

    Primeiro commit

--- src/js/script.js
+++ src/js/script.js
@@ -26,11 +26,42 @@ const users = [
     }
 ];
 
-function ValidateForm() {
+function ValidateForm(event) {
+    
+    event.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('passwd').value
+    const msg = document.getElementById('msg');
+
+
+    function validateEmail() {
+        if(email.target != ' ') {
+            msg.innerHTML = ' ';
+        } else {
+            msg.innerHTML ='O campo email deve ser preenchido!'
+        }
+    }
+    validateEmail()
+        
+    /*function validaLogin() {
+        const user = users.find(u => u.email === email);
+        
+        if (user != undefined && user.email == email) {  
+            if (password == 127) {
+                console.log('Sucesso!');
+                window.alert('Seja bem vindo!');
+            } else {
+                window.alert('Senha inválida! Tente novamente');
+            }
+        } else {
+           // window.alert('Usuário não encontrado!');
+        }
+    }
+    validaLogin();
     /*
-        1. Referenciar elementos dos campos do formulário pelo ID
-        2. Verificar se a senha e o e-mail estão válidos 
-        3. Se tudo estiver ok, alertar o usuário que o formulário foi submetido
+        1. Referenciar elementos dos campos do formulário pelo ID;
+        2. Verificar se a senha e o e-mail estão válidos;
+        3. Se tudo estiver ok, alertar o usuário que o formulário foi submetido;
 
         Plus: Alertar o usuário quando o e-mail ou senha estiverem vazios ou incorretos.
     */
